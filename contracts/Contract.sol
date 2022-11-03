b1966dadd694921910318aa32787557ba64a5d2e
darahask <darahask12@gmail.com>
2022-11-03 22:51:48 +0530

    coolnft

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC721Base.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 
-contract Contract is ERC721Base {
+contract Contract is ERC721Base, PermissionsEnumerable {
 
-      constructor(
+    mapping(uint256 => uint256) public powerLevel;
+    constructor(
         string memory _name,
         string memory _symbol,
         address _royaltyRecipient,
@@ -17,6 +19,21 @@ contract Contract is ERC721Base {
             _royaltyRecipient,
             _royaltyBps
         )
-    {}
+    {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function setPowerLevel(uint256 _tokenId,
+                 uint256 _powerLevel) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        powerLevel[_tokenId] = _powerLevel;
+    }
+
+    function mintTo(address _to,
+                string memory _tokenURI) public virtual override {
+        uint256 tokenId = nextTokenIdToMint();
+
+        super.mintTo(_to, _tokenURI);
+        powerLevel[tokenId] = tokenId;
+    }
 
 }
